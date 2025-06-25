35e161ba033919498c061060ce9a7e55db1689a7
SvelteRust <oddharald@myhren.ai>
2025-06-25 19:26:10 +0200

    nice

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ use eframe::egui;
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions::default();
-    eframe::run_native("App", options, Box::new(|_| Ok(Box::<App>::default())))
+    eframe::run_native(
+        "Window",
+        options,
+        Box::new(|cc| {
+            // Setup App
+            cc.egui_ctx.set_visuals(egui::Visuals::light());
+            let app = App { age: 0 };
+            Ok(Box::new(app))
+        }),
+    )
 }
 
-#[derive(Default)]
 struct App {
     age: usize,
 }
@@ -13,7 +21,7 @@ struct App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("App");
+            ui.heading("Window");
             ui.add(egui::Slider::new(&mut self.age, 0..=100).text("age"));
             if ui.button("Increment").clicked() {
                 self.age += 1;
